80a0f937b330dbd2ec52e7e1abe8cfffbe0ea44c
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2022-09-11 18:13:57 +0500

    Add files via upload

---
+++ Robert Lafore Book Solution/CH 2 Exercise/Q1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float x;
+cout<<"Enter Gallons: ";
+cin>>x;
+cout<<"The Equivalent of "<<x<<" Gallons in Cubic Feet is: "<<x/7.481;
+
+return 0;
+}
